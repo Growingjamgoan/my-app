0df6797c1d54a57284bcacab36755119e14e206b
kumardil91 <91dileeppandey@gmail.com>
2018-10-30 16:41:51 +0530

    Update Calculator.java

--- src/main/java/in/javahome/myweb/controller/Calculator.java
+++ src/main/java/in/javahome/myweb/controller/Calculator.java
@@ -8,10 +8,10 @@ public class Calculator {
 	 * @param j
 	 * @return int
 	 */
-	public int add(int i, int j){
+	public String add(){
 		String name = "hello";
 		name.toUpperCase();
-		return i+j;
+		return name;
 	}
 	public int multiply(int i, int j){
 		return i*j;
